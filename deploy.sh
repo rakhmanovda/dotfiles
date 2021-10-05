21a51d2176e8a01a3c4c745d2a0e56cadc2a3753
rakhmanovda <denisrakhmanov@gmail.com>
2021-10-05 14:10:57 +0200

    update configs

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ln -f bash/.bashrc ~/.bashrc
+ln -f xmonad/xmonad.hs ~/.xmonad/xmonad.hs
+ln -f polybar/config ~/.config/polybar/config
+ln -f polybar/launch.sh ~/.config/polybar/launch.sh
+ln -f autostart/autostart.sh ~/.config/autostart.sh
