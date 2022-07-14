327fca8269a1af568f7d2fdadd1ee7252256b287
rakhmanovda <denisrakhmanov@gmail.com>
2022-07-14 21:17:42 +0200

    removing unused

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ln -f bash/.bashrc ~/.bashrc
-ln -f xmonad/xmonad.hs ~/.xmonad/xmonad.hs
-ln -f polybar/config ~/.config/polybar/config
-ln -f polybar/launch.sh ~/.config/polybar/launch.sh
-ln -f autostart/autostart.sh ~/.config/autostart.sh
-ln -f rofi/config.rasi ~/.config/rofi/config.rasi
